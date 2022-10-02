eabdf854ec5bbdff862093a3b1a2c6456ab97eb4
Ryan Sheehan <rsheehan@gmail.com>
2022-10-02 11:59:45 -0500

    added open-props

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,12 @@
 const autoprefixer = require("autoprefixer");
+const postcssJitProps = require('postcss-jit-props');
+const OpenProps = require('open-props');
 
 const config = {
-  plugins: [autoprefixer],
+  plugins: [
+    postcssJitProps(OpenProps),
+    autoprefixer,
+  ],
 };
 
 module.exports = config;
